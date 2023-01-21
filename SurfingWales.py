f975347fda19641f447df5c77a39a26ca7bbcac0
Pharmer Palmer <pharmerpalmer@gmail.com>
2023-01-21 20:32:31 +0000

    update removing old config - ready for new loop

--- SurfingWales.py
+++ SurfingWales.py
@@ -4,15 +4,11 @@
 def surf_spots(my_location):
     with open("beach_break.txt") as f:
         f = f.splitlines()
-
-county = input("enter gower: ")
-with open("surf_wales.txt") as f: #get it and open it
-    show_arp = f.read()         #read it and assign it
-
-print()
-for line in show_arp.splitlines(): #makes the string into a list of lines,
-    fields = line.split() #makes the line element into a list of words
-    print(fields)
+        
+        print()     
+        for line in show_arp.splitlines(): #makes the string into a list of lines,
+        fields = line.split() #makes the line element into a list of words
+        print(fields)
     '''
     county = fields[0] #assigns the index 0 element of each line into variable county
     area = fields[1] #assigns the index 1 element of each line into variable area
