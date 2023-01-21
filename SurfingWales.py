ec95595f978c747b1b138a4c7691b5a90d5139f4
Pharmer Palmer <pharmerpalmer@gmail.com>
2023-01-21 18:13:10 +0000

    update the .py file for the first time added lines defining a function

--- SurfingWales.py
+++ SurfingWales.py
@@ -1,4 +1,9 @@
 #Surfing wales advise
+#def a functin which when called and passed an argument from input will open then read the text file then loo through digging ""==" out:
+
+def surf_spots(my_location):
+    with open("beach_break.txt") as f:
+        f = f.splitlines()
 
 county = input("enter gower: ")
 with open("surf_wales.txt") as f: #get it and open it
@@ -17,3 +22,6 @@ for line in show_arp.splitlines(): #makes the string into a list of lines,
     if county == "gower":
         print("Default gateway IP/Mac is: {}/{}".format(county, area)) # string format to print the above 2 variables
 print()'''
+
+my_location = input("Where are you currently? ")
+print((surf_spots(my_location)))
